3dac2e29b760ec6163d78dd05679db5016cfae70
Antony Male <antony.male@gmail.com>
2014-01-27 13:01:24 +0000

    Work towards editing friends

---
+++ Validation/ValidationBindingExtension.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Ec2Manager.Validation
+{
+    public class ValidationBindingExtension : MarkupExtension
+    {
+        [ConstructorArgument("Path")]
+        public PropertyPath Path { get; set; }
+
+        public bool ReplaceInvalidValues { get; set; }
+        public object ReplaceInvalidValuesWith { get; set; }
+
+        public IValueConverter Converter { get; set; }
+        public string StringFormat { get; set; }
+        public UpdateSourceTrigger UpdateSourceTrigger { get; set; }
+
+        public ValidationBindingExtension(string path)
+        {
+            this.Path = new PropertyPath(path);
+
+            this.ReplaceInvalidValues = true;
+            this.ReplaceInvalidValuesWith = null;
+
+            this.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var binding = new Binding(this.Path.Path);
+
+            binding.ValidatesOnDataErrors = true;
+            binding.ValidatesOnExceptions = true;
+
+            binding.UpdateSourceTrigger = this.UpdateSourceTrigger;
+            binding.StringFormat = this.StringFormat;
+
+            if (this.ReplaceInvalidValues)
+            {
+                var converter = new ReplaceInvalidValuesConverter();
+                converter.ReplaceInvalidValuesWith = this.ReplaceInvalidValues;
+
+                if (this.Converter == null)
+                    binding.Converter = converter;
+                else
+                    binding.Converter = new ValueConverterGroup() { this.Converter, converter };
+            }
+            else
+            {
+                binding.Converter = this.Converter;
+            }
+
+            return binding.ProvideValue(serviceProvider);
+        }
+    }
+
+    public class ReplaceInvalidValuesConverter : DependencyObject, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var converter = TypeDescriptor.GetConverter(targetType);
+            if (!converter.IsValid(value))
+                return null;
+
+            var convertedValue = converter.ConvertFrom(value);
+            return convertedValue;
+        }
+
+        public object ReplaceInvalidValuesWith
+        {
+            get { return (object)GetValue(ReplaceInvalidValuesWithProperty); }
+            set { SetValue(ReplaceInvalidValuesWithProperty, value); }
+        }
+
+        public static readonly DependencyProperty ReplaceInvalidValuesWithProperty =
+        DependencyProperty.Register("ReplaceInvalidValuesWith", typeof(object), typeof(ReplaceInvalidValuesConverter), new PropertyMetadata(null));
+    }
+
+    // http://stackoverflow.com/a/8326207/1086121
+    public class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.ConvertBack(current, targetType, parameter, culture));
+        }
+    }
+}
--- ViewModels/ManageFriendsViewModel.cs
+++ ViewModels/ManageFriendsViewModel.cs
@@ -1,23 +1,75 @@
 ﻿using Caliburn.Micro;
 using Ec2Manager.Configuration;
+using Ec2Manager.Validation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ec2Manager.ViewModels
 {
-    public class ManageFriendsViewModel
+    public class ManageFriendsViewModel : Screen
     {
         private Config config;
 
-        public BindableCollection<Friend> Friends { get; private set; }
+        public BindableCollection<FriendModel> Friends { get; private set; }
 
         public ManageFriendsViewModel(Config config)
         {
+            this.DisplayName = "Manage Friends";
+
             this.config = config;
-            this.Friends = new BindableCollection<Friend>(config.FriendsWithoutDefaults);
+            this.Friends = new BindableCollection<FriendModel>(config.FriendsWithoutDefaults.Select(x => new FriendModel(x)));
+        }
+
+        
+    }
+
+    public class FriendModel : PropertyChangedBase, IDataErrorInfo, IValidationProvider
+    {
+        private IValidator validator = new Validator();
+
+        public string Name { get; set; }
+        public string UserId { get; set; }
+
+        public FriendModel()
+        {
+            this.ValidateWith(() => this.Name, x => false, "Bad something or other");
+        }
+
+        public FriendModel(Friend friend) : this()
+        {
+            this.Name = friend.Name;
+            this.UserId = friend.UserId;
+        }
+
+        public IValidation ValidateWith<TProperty>(System.Linq.Expressions.Expression<Func<TProperty>> property, Func<TProperty, bool> validator, string message)
+        {
+            return this.validator.ValidateWith(property, validator, message);
+        }
+
+        public string Error
+        {
+            get { return String.Join(Environment.NewLine, this.validator.Errors); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (this.validator.IsPropertyChecked(columnName))
+                {
+                    var errors = String.Join(Environment.NewLine, this.validator.CheckProperty(columnName));
+                    this.NotifyOfPropertyChange(() => this.Error);
+                    return errors;
+                }
+                else
+                {
+                    return "";
+                }
+            }
         }
     }
 }
